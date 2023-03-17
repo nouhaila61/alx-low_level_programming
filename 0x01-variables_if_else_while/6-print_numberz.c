ac971786f86d2bd09b0c0432153fb76f829b1fe7
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-17 13:44:24 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers
+ *
+ * Return: 0
+*/
+
+	int main(void)
+{
+
+	int x;
+
+	for (x = 48; x <= 57; x++)
+{
+	putchar(x);
+}
+	putchar('\n');
+	return (0);
+}
