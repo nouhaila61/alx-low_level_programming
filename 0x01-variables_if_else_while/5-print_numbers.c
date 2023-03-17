038e6d65f99419306894dafb043c9b8cb65a0757
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-17 13:11:11 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints tha alphabet in lowercase
+ *
+ * Return: 0
+*/
+
+	int main(void)
+{
+
+	char x;
+
+	for (x = 0; x < 10; x++)
+{
+	printf("%d\n", x);
+}
+	return (0);
+}
