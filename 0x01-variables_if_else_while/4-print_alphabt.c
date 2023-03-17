f76121cb81d2d07aa329f4728b08d8f4484422f9
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-17 12:30:07 -0700

    alphABETe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints tha alphabet in lowercase
+ *
+ * Return: 0
+*/
+
+        int main(void)
+{
+
+        char x;
+
+        for (x = 'a'; x <= 'z'; x++)
+{
+	if (letter != 'e' && letter != 'q')
+	putchar (x);
+} 
+        putchar ('\n');
+        return (0);
+}
