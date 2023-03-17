0ca652c0440bdfab5aa4dee1713e5a2ecd7058b3
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-17 10:52:35 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,13 @@
  * Return: 0
 */
 
-int main() {
-        char x;
-	for (x = 'a'; x <= 'z'; x++) {
-	putchar(x);
-    }
-    putchar('\n');
-    return (0);
+	int main(void)
+{
+
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	putchar (x);
+
+	putchar ('\n');
+	return (0);
 }
