5b0777782b6ed4b0c7e7171b76e069f8035fed7e
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-21 11:12:01 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower -  display 1 if the alphabet, in lowercase or uppercase
+ * @c: parameter
+ * Return: 1 if c is lowercase or uppercase else 0
+ *
+ */
+int _isalpha(int c)
+{
+
+	if ((c >= 97 && c <= 122) || (c >= 66 && c <90))
+	{
+		return (1);
+	}
+	else
+	return (0);
+_putchar('\n');
+}
