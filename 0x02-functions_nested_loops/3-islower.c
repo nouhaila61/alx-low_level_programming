649cdc169ac7717dac3e610ac3774fa17c303eff
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-21 10:27:59 -0700

    alphabetl

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,20 +3,18 @@
 /**
  * _islower -  display 1 if the alphabet, in lowercase
  * @c: parameter
- * Return: 1 if c is lowercase
- * Return: 0 otherwise
+ * Return: 1 if c is lowercase else 0
  *
  */
 int _islower(int c)
+{
 
 	if (c >= 97 && c <= 122)
-{
+	{
 		return (1);
-}
-else
-{
-		return (0);
-}
-	_putchar('\n');
+	}
+	else
+	return (0);
+_putchar('\n');
 }
 
