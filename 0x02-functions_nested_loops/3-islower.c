a3d2fccc10f50cddf59f0079687d8517d06ad74f
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-21 10:18:36 -0700

    alphabetl

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower -  display 1 if the alphabet, in lowercase
+ * @c: parameter
+ * Return: 1 if c is lowercase
+ * Return: 0 otherwise
+ *
+ */
+int _islower(int c)
+
+	if (c >= 97 && c <= 122)
+{
+		return (1);
+}
+else
+{
+		return (0);
+}
+	_putchar('\n');
+}
+
