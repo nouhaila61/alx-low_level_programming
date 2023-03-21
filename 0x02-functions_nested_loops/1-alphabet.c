8a45070018590afec6dfc7e80bcc961929dc781c
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-21 08:24:42 -0700

    aphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the alphabet, in lowercase
+ *
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+		_putchar(x);
+	_putchar('\n');
+}
+
