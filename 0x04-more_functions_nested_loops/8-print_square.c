8de30a90bc70aac04235ffb83de8188a24b58ded
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-23 11:15:14 -0700

    yeol

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,18 +7,18 @@
   */
 void print_square(int size)
 {
-	int x, y;
 
 	if (size <= 0)
 
 		_putchar('\n');
 
+	int x, y;
 		for (x = 0; x < size; x++)
 		{
 		for (y = 0; y < size; y++)
-
+{
 		_putchar(35);
-
+}
 	_putchar('\n');
 		}
 }
