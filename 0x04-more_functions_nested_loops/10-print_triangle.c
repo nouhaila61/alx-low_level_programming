e2c4880286a41b63a0c35a620d3da6539ddaad4c
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-23 16:58:31 -0700

    yeolll

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle n times large
+ * @size: the size of the triangle
+ * description: prints a square n times large using _putchar
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int a;
+	int n;
+	int z;
+
+	n = size;
+	i = 1;
+	a = 1;
+	z = 0;
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		while (i < size + 1)
+		{
+			a = z;
+			while ((n - a) - 1 > 0)
+			{
+				_putchar(' ');
+				a++;
+			}
+			a = z;
+			while (a >= 0)
+			{
+				_putchar('#');
+				a--;
+			}
+			z++;
+			i++;
+			_putchar('\n');
+		}
+	}
+}
+
+
