d809437cd163dc78754f3113fcc587832c3eea09
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-23 13:52:44 -0700

    buzzzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  * main - Prints Fizz Buzz from 1 to 100
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+
+		printf("FizzBuzz ");
+
+	else if (i % 3 == 0)
+
+	printf("Fizz ");
+
+	else if (i % 5 == 0)
+
+	printf("Buzz ");
+
+	else
+
+	printf("%d ", i);
+	i++;
+}
+
+	printf("\n");
+
+	return (0);
+}
+
+
