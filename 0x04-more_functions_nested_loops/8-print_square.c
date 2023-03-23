73709bc23bef18b55b99258133b5d859207086d5
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-23 11:07:17 -0700

    yeol

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_square - prints a square
+  * @size: The number of lines to draw
+  * Return: empty
+  */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+
+		_putchar('\n');
+
+		for (x = 0; x < n; x++)
+		{
+		for (y = 0; y < x; y++)
+
+		_putchar(35);
+
+	_putchar('\n');
+		}
+}
+
