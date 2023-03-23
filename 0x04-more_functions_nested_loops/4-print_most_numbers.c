030d4ff4139c0e7a2af19b09efcf398c7f8261fa
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-23 08:45:05 -0700

    yeol

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  prints the numbers, from 0 to 9 , except 3 and 6
+ * print_most_numbers -  prints the numbers, from 0 to 9 , except 2 and 4
  */
 void print_most_numbers(void)
 {
