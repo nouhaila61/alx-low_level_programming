afce0ec5ee7b1333b0fd9bcafb978a9451f0902f
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-23 08:41:45 -0700

    yeol

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9 , except 3 and 6
+ */
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (i == '3' || i == '6')
+
+		continue;
+
+	_putchar(x);
+	}
+
+		_putchar('\n');
+}
+
