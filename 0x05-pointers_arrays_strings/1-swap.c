b973a9beac5067ffe2ca32db8d0d787cc7647cf6
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-27 08:41:48 -0700

    yeol

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: Pointer of value 98
+ * @b: Pointer of value 42
+ * Return: voi
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	int swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
