368489a6554ef3ac49ce2371be6d4ece30c55e88
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-03-27 10:06:55 -0700

    yeol

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a given string in reverse
+ * @str: the string
+ */
+
+void print_rev(char *str)
+{
+	int len = 0;
+
+	while (s[len])
+	{
+		len++;
+	}
+
+	for (int i = len - 1; i >= 0; i--)
+	_putchar(s[i]);
+
+	_putchar('\n');
+}
+
