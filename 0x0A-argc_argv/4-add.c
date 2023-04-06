9ef9d6f451dcae8549bc1fe20373e90f72681be2
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-06 15:05:21 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - Write a program that adds positive numbers
+ * @argc: count the number of argruments.
+ * @argv: Pinter to the string of arguments.
+ * Return: 0.
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) > 0)
+			{
+				sum += atoi(argv[i]);
+			}
+		else
+			{
+				printf("Error\n");
+				return (1);
+			}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
