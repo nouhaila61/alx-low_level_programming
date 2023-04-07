74f60e9c4928c615486f93da36aec83fb9f1993f
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-07 08:02:27 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - prints the number of arguments passed into it, followed by a new line.
+ * main - prints the number of arguments passed into it
  * @argc: count the number of argruments.
  * @argv: Pinter to the string of arguments.
  * Return: 0.
@@ -11,6 +11,6 @@ int main(int  argc, __attribute__((unused)) char *argv[])
 
 	printf("%d\n", argc - 1);
 
-	return (0);	
+	return (0);
 }
 
