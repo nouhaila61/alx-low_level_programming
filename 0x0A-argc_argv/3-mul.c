03b0c11eedfe87572e36a01fdd55401c03e1250c
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-07 08:08:40 -0700

    multi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,17 +8,17 @@
 */
 int main(int argc, char *argv[])
 {
+	int num1, num2, sum;
+
 	if (argc != 3)
 	{
 	printf("Error\n");
 	return (1);
 	}
 
-	int num1, num2, sum;
-
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	sum = numb1 * numb2;
+	sum = num1 * num2;
 
 	printf("%d\n", sum);
 	return (0);
