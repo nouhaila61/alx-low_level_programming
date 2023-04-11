c6ff5c3c4707d83f0da6e07a7caee704962afc8c
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-11 16:27:27 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < len1; i++)
 		concat_str[i] = s1[i];
 	for (j = 0; j < len2; j++)
-		concat_str[i+j] = s2[j];
+		concat_str[i + j] = s2[j];
 	concat_str[i+j] = '\0';
 
 	return (concat_str);
