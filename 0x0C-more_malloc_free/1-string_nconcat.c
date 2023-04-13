d33cb39d4759d0d9a1b58f6e2f4dd7f04410570c
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-13 09:24:01 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < len1; i++)
 		concat_str[i] = s1[i];
-	for (j = 0; j < len2; j++)
+	for (j = 0; j < n; j++)
 		concat_str[i + j] = s2[j];
 	concat_str[i + j] = '\0';
 
