f7e7429a46874f2a01ca32b301c0ec733edf4b9f
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-13 18:03:44 -0700

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * __memcpy - copies memory area.
+ * @dest: memory area to copy to.
+ * @src: memory area to copy from.
+ * @n: bytes of memory.
+ * Return: a pointer to dest.
+ */
+
+char *__memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
+}
