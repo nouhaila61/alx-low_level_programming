df47d6308dbccbd61bb050aedc1f8fd707ac8757
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-19 23:53:13 +0000

    operator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,13 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (*(ops[i].op) == *s)
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
-	return (NULL);
+
+	return (0);
+
 }
