cc01cd41a92cf4d150e5c15445df04c08d1ceaf1
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-19 19:15:15 +0000

    operators

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * main - program that perfroms simple operations
+ * @ac: number of arguments
+ * @av: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int (*oprt)(int, int);
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = oprt(atoi(argv[1]), atoi(argv[3]))
+	printf("%d\n", result);
+
+	return (0);
+}
