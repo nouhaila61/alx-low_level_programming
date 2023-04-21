171b8b073029e975f2eb005f6005093908cd49ef
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-21 00:18:42 +0000

    print spring

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,8 +17,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		if (i + 1 != n && separator != NULL)
 			printf("%c%s", va_arg(arg, int), separator);
-		else if (va_arg(arg, char *) == NULL)
-			printf("(nil)");
 		else
 			printf("%c", va_arg(arg, int));
 	}
